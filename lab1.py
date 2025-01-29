a850dfc7ef80ed4550f1cec163b433a8ff32caf1
ShyamSundar-07 <shyamgrand005@gmail.com>
2025-01-29 11:57:04 +0530

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,15 @@
+a=[78,80,45,7,89,90,100,3,67,48]
+max=a[0]
+min=a[0]
+for i in a:
+ if i>max:
+   max=i
+if i < min:
+ min=i
+print("the largest number is : ",max)
+print("the smallest number is : ",min)
+
+
+ 
+  
+  
